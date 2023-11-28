312fb8e387f4b5c7cf4d0b47d2d23793d3d1e57d
unknown <lauravalenciapolo@gmail.com>
2023-11-27 19:42:08 -0500

    prueba 2

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Laura Valencia</h1>
+      <h1>Laura Valencia Poloooooo </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
